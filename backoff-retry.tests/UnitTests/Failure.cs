4ae1cada9d213ae43e31d2e1af646e154483d7db
Adam Feather <adamfeather@gmail.com>
2018-06-19 20:47:14 +0100

    Added success and failure tests

--- backoff-retry.tests/UnitTests/Failure.cs
+++ backoff-retry.tests/UnitTests/Failure.cs
@@ -10,13 +10,14 @@ namespace backoff_retry.tests.UnitTests
         [TestCase(1)]
         [TestCase(3)]
         [TestCase(5)]
+        [TestCase(6)]
         public void Should_Fail_Initital_Attempt_And_Each_Explictely_Specified_Backoff_Retry(int maxAttempts)
         {
             Action failingFunction = () => throw new Exception("function failed");
 
             var backoffRetry = new BackoffRetry(failingFunction);
 
-            bool result = backoffRetry.AttemptExponential(maxAttempts, TimeSpan.FromMilliseconds(100));
+            bool result = backoffRetry.AttemptExponential(maxAttempts, TimeSpan.FromMilliseconds(10));
 
             Assert.That(result, Is.False);
             Assert.That(backoffRetry.RetryAttempts, Is.EqualTo(maxAttempts));
--- backoff-retry.tests/UnitTests/Success.cs
+++ backoff-retry.tests/UnitTests/Success.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using NUnit.Framework;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace backoff_retry.tests.UnitTests
 {
+    [TestFixture]
     public class Success
     {
+        [Test]
+        public void Should_Suceed_On_First_Attempt()
+        {
+            Action successfulFunction = () => { Thread.Sleep(100); };
+
+            var backoffRetry = new BackoffRetry(successfulFunction);
+
+            bool result = backoffRetry.AttemptExponential(10, TimeSpan.FromMilliseconds(10));
+
+            Assert.That(result, Is.True);
+            Assert.That(backoffRetry.RetryAttempts, Is.EqualTo(0));
+            Assert.That(backoffRetry.Exceptions.Count(), Is.EqualTo(0));
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(5)]
+        public void Should_Fail_First_Attempt_And_Then_Succeed_After_Specified_Retry(int succeedOnRetry)
+        {
+            bool intialAttempt = true;
+            int retry = 0;
+
+            Action successfulFunction = () =>
+            {
+                if (intialAttempt)
+                {
+                    intialAttempt = false;
+                    throw new Exception("Fail on first attempt");
+                }
+
+                if (retry++ < succeedOnRetry)
+                {
+                    throw new Exception("Not time to succeed yet");
+                }
+            };
+
+            var backoffRetry = new BackoffRetry(successfulFunction);
+
+            bool result = backoffRetry.AttemptExponential(10, TimeSpan.FromMilliseconds(10));
+
+            Assert.That(result, Is.True);
+            Assert.That(backoffRetry.RetryAttempts, Is.EqualTo(succeedOnRetry));
+            Assert.That(backoffRetry.Exceptions.Count(), Is.EqualTo(succeedOnRetry + 1));
+        }
     }
 }
