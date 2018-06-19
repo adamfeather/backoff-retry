34146baafbf91e1841782aa2949b4ff8cfd1de4d
Adam Feather <adamfeather@gmail.com>
2018-06-19 08:40:24 +0100

    inital commit

---
+++ backoff-retry/BackoffRetry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace backoff_retry
+{
+    public class BackoffRetry
+    {
+        public BackoffRetry()
+        {
+        }
+
+        public bool Attempt()
+        {
+            var list_of_integers = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            IEnumerable<Foo> result =
+                list_of_integers
+                    .Where(GreaterThanFive)
+                    .Select(NewFooObject);
+
+            return true;
+        }
+
+        private Foo NewFooObject(int i)
+        {
+            return new Foo(i);
+        }
+
+        private static bool GreaterThanFive(int i)
+        {
+            return i > 5;
+        }
+    }
+}
---
+++ backoff-retry/Foo.cs
@@ -0,0 +1,12 @@
+﻿namespace backoff_retry
+{
+    internal class Foo
+    {
+        private readonly int i;
+
+        public Foo(int i)
+        {
+            this.i = i;
+        }
+    }
+}
